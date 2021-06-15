306bcbfb400c9d13851e01ad567fab9bd491bb01
Trevor <mangrumtrevor@gmail.com>
2021-06-15 00:53:19 -0500

    Fixes linter issues

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,9 +1,7 @@
 import React from "react";
 import { NextPage } from "next";
 const HomePage: NextPage = () => {
-    return(
-        <h1>Hello world!</h1>
-    )
-}
+  return <h1>Hello world!</h1>;
+};
 
 export default HomePage;
