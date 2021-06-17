0e6535ebf63cc8fed3b980246f8a2cd357950700
Trevor Mangrum <tmangrum@Trevors-MacBook-Air.local>
2021-06-17 14:49:03 -0500

    Implements footer styles

--- src/components/Footer/index.tsx
+++ src/components/Footer/index.tsx
@@ -1,1 +1,58 @@
-export {};
+import React from "react";
+
+const Footer: React.FC = () => {
+  return (
+    <footer className="footer">
+      <hr />
+      <section className="footer-wrapper">
+        <div className="footer-section">
+          <h3>The Backdrop.</h3>
+          <a href="">Pricing</a>
+          <a href="">Gallery</a>
+          <a href="">Book</a>
+        </div>
+        <div className="footer-section">
+          <h3>About.</h3>
+          <a href="#">About Us</a>
+          <a href="#">Contact Us</a>
+          <a href="#">Privacy Policy</a>
+        </div>
+        <div className="footer-section">
+          <h3>Hours.</h3>
+          <div>
+            <span>Thursday:</span>
+            <span>2:00PM-8:00PM</span>
+          </div>
+          <div>
+            <span>Fri-Sat:</span>
+            <span>2:00PM-9:00PM</span>
+          </div>
+          <div>
+            <span>Sunday:</span>
+            <span>1:00PM-5:00PM</span>
+          </div>
+        </div>
+        <div className="footer-section">
+          <h3>Contact.</h3>
+          <address>
+            <span>102 B West Court Avenue, Selmer TN, 38375</span>
+            <a href="tel:7314347149">731.434.7149</a>
+            <a href="mailto:thebackdrop731@gmail.com">
+              thebackdrop731@gmail.com
+            </a>
+          </address>
+        </div>
+      </section>
+      <section className="footer-wrapper">
+        <span>
+          Website by:{" "}
+          <a target="blank" rel="noreferrer" href="https://mangrumtech.com">
+            Mangrum Tech
+          </a>
+        </span>
+      </section>
+    </footer>
+  );
+};
+
+export default Footer;
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -2,12 +2,15 @@ import React from "react";
 import { NextPage } from "next";
 import Button from "src/components/Button";
 import Header from "src/components/Header";
+import Footer from "src/components/Footer";
 const HomePage: NextPage = () => {
   return (
-  <div>
-    <Header />
-    <Button href='/' text="view"/>
-  </div>);
+    <div>
+      <Header />
+      <Button href="/" text="view" />
+      <Footer />
+    </div>
+  );
 };
 
 export default HomePage;
