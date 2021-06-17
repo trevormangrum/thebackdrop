4646a94efa5e87b6a31d23393216548442bf1a8f
Trevor Mangrum <tmangrum@Trevors-MacBook-Air.local>
2021-06-17 13:27:33 -0500

    Adds button styling

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,7 +1,11 @@
 import React from "react";
 import { NextPage } from "next";
+import Button from "src/components/Button";
 const HomePage: NextPage = () => {
-  return <h1>Hello world!</h1>;
+  return (
+  <div>
+    <Button href='/' text="view"/>
+  </div>);
 };
 
 export default HomePage;
