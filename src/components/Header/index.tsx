34e269bad1e34a130c38f41e3c135c68f861eb24
Trevor Mangrum <tmangrum@Trevors-MacBook-Air.local>
2021-06-17 14:08:46 -0500

    Implements header

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -1,1 +1,29 @@
-export {};
+import React from "react";
+import Button from "src/components/Button";
+
+const Header: React.FC = () => {
+    const [mobileOpen, setMobileOpen] = React.useState(false);
+    return (
+        <header className="navigation">
+            <div className="nav-wrapper">
+                <a href="/" className="logo">
+                    The Backdrop
+                </a>
+                <nav className={`navigation-links ${mobileOpen ? "mobile-open" : "mobile-hidden"}`}>
+                    <a href="#">Home</a>
+                    <a href="#">About</a>
+                    <a href="#">Contact</a>
+                    <a href="#">Pricing</a>
+                    <Button href='/book' text="Book"/>
+                </nav>
+                <div className="nav-button" onClick={() => setMobileOpen(!mobileOpen)}>
+                    <div></div>
+                    <div></div>
+                    <div></div>
+                </div>
+            </div>
+        </header>
+    );
+};
+
+export default Header;
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,9 +1,11 @@
 import React from "react";
 import { NextPage } from "next";
 import Button from "src/components/Button";
+import Header from "src/components/Header";
 const HomePage: NextPage = () => {
   return (
   <div>
+    <Header />
     <Button href='/' text="view"/>
   </div>);
 };
