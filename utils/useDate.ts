4e56c6acc870ffceafad470c7f5f50b9d4252de6
Trevor Mangrum <mangrumtrevor@gmail.com>
2021-09-28 00:30:07 -0400

    Updates useDate

--- utils/useDate.ts
+++ utils/useDate.ts
@@ -3,7 +3,7 @@ import { IDate } from "utils/types";
 export default function useDate(date: IDate) {
   const fetcher = (...args) => fetch(...args).then((res) => res.json());
   const { data, error } = useSWR(
-    `/api/booking?date=${date ? date instanceof Date && (date as Date).toLocaleString("en-US", {day: "numeric", month: "numeric", year: "numeric"}) : ""}`,
+    `/api/booking?date=${date ? date instanceof Date && (date as Date).toDateString() : ""}`,
     fetcher
   );
   return {
